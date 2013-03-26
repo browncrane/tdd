9f99fa9da0b6d2f460258b7eb6551a070d05f10b
browncrane <im2busy@qq.com>
2013-03-26 17:21:15 +0800

    finish share multiple number

---
+++ src/test/java/org/thoughtworks/app/MoneyTest.java
@@ -0,0 +1,15 @@
+package org.thoughtworks.app;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class MoneyTest{
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+}
