037efb8c486ec54d4b39392fa86bce623e71c2f0
browncrane <im2busy@qq.com>
2013-03-27 10:50:18 +0800

    finish Dollar equals function

--- src/test/java/org/thoughtworks/app/MoneyTest.java
+++ src/test/java/org/thoughtworks/app/MoneyTest.java
@@ -1,15 +1,26 @@
 package org.thoughtworks.app;
 
+
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 
 public class MoneyTest{
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10,five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10,product.amount);
+        product = five.times(3);
+        assertEquals(15,product.amount);
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
