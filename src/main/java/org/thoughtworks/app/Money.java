218fb804c60e2e53db107bda319ea0712f8d6342
browncrane <im2busy@qq.com>
2013-03-28 16:46:16 +0800

    Delete Dollar and Franc

--- src/main/java/org/thoughtworks/app/Money.java
+++ src/main/java/org/thoughtworks/app/Money.java
@@ -15,6 +15,14 @@ public class Money {
         this.currency = currency;
     }
 
+    static Money dollar(int amount){
+        return new Money(amount,"USD");
+    }
+
+    static Money franc(int amount){
+        return new Money(amount,"CHF");
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
